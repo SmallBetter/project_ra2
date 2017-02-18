64c30b36d7e9806822e2dffecf59c25c12d2e649
SmallBetter <forsakenevil0811@gmail.com>
2017-02-18 15:10:00 +0700

    fix modulet

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello world")
+var trace = require('tracer').colorConsole({level: 'error'})
+trace.info('This is info!!!')
+trace.warn('This is warning!!!')
+trace.error('This is error!!!')
